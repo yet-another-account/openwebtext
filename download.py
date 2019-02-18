13cd6c77fccda34c9718ad208e113168524b593b
eukaryote <eukaryote31@gmail.com>
2019-02-18 10:40:39 -0700

    Extract filter

--- download.py
+++ download.py
@@ -5,6 +5,7 @@ import hashlib
 import traceback
 import tldextract
 import tqdm
+from filter import should_exclude
 
 hash = hashlib.sha256
 
@@ -14,78 +15,14 @@ except FileExistsError:
     pass
 
 
-# domains that aren't scraper friendly. do not include subdomains!
-exclude_domains = set([
-    # image & video hosting sites
-    'imgur.com',
-    'redd.it',
-    'gfycat.com',
-    'giphy.com',
-    'reddituploads.com',
-    'redditmedia.com',
-    'twimg.com',
-    'sli.mg',
-    'magaimg.net',
-    'flickr.com',
-    'imgflip.com',
-    'youtube.com',
-    'youtu.be',
-    'youtubedoubler.com',
-    'vimeo.com',
-    'twitch.tv',
-    'streamable.com',
-    'bandcamp.com',
-    'soundcloud.com',
-
-    # not scraper friendly
-
-    'reddit.com',
-    'gyazo.com',
-    'github.com',
-    'xkcd.com',
-    'twitter.com',
-    'spotify.com',
-    'itunes.apple.com',
-    'facebook.com',
-    'gunprime.com',
-    'strawpoll.me',
-    'voyagefusion.com',
-    'rollingstone.com',
-    'google.com',
-    'timeanddate.com',
-    'nfl.com',
-
-    # original paper excluded wikipedia
-    'wikipedia.org',
-
-    # lots of top posts for this one
-    'battleforthenet.com',
-])
-
-exclude_extensions = (
-    '.png',
-    '.jpg',
-    '.jpeg',
-    '.gif',
-    '.gifv',
-    '.pdf',
-    '.mp4',
-    '.mp3',
-    '.ogv',
-    '.webm',
-)
-
-
 def dl(url):
     url = url.strip()
 
+    if should_exclude(url):
+        continue
+
     ext = tldextract.extract(url)
     domain = '.'.join([x for x in ext if x])
-    if '.'.join(ext[-2:]) in exclude_domains or domain in exclude_domains:
-        return
-
-    if url.split('?')[0].endswith(exclude_extensions):
-        return
 
 #    print('Downloading', url)
     try:
@@ -107,6 +44,7 @@ def dl(url):
 
     with open(fname, 'w') as out:
         out.write(text)
+
 
 if __name__ == '__main__':
     p = mp.Pool(100) # num of download threads
---
+++ filter.py
@@ -0,0 +1,90 @@
+import tldextract
+import tqdm
+
+
+# domains that aren't scraper friendly. do not include subdomains!
+exclude_domains = set([
+    # image & video hosting sites
+    'imgur.com',
+    'redd.it',
+    'gfycat.com',
+    'giphy.com',
+    'reddituploads.com',
+    'redditmedia.com',
+    'twimg.com',
+    'sli.mg',
+    'magaimg.net',
+    'flickr.com',
+    'imgflip.com',
+    'youtube.com',
+    'youtu.be',
+    'youtubedoubler.com',
+    'vimeo.com',
+    'twitch.tv',
+    'streamable.com',
+    'bandcamp.com',
+    'soundcloud.com',
+
+    # not scraper friendly
+
+    'reddit.com',
+    'gyazo.com',
+    'github.com',
+    'xkcd.com',
+    'twitter.com',
+    'spotify.com',
+    'itunes.apple.com',
+    'facebook.com',
+    'gunprime.com',
+    'strawpoll.me',
+    'voyagefusion.com',
+    'rollingstone.com',
+    'google.com',
+    'timeanddate.com',
+    'walmart.com',
+    'roanoke.com',
+    'spotrac.com'
+
+    # original paper excluded wikipedia
+    'wikipedia.org',
+
+    # lots of top posts for this one
+    'battleforthenet.com',
+])
+
+exclude_extensions = (
+    '.png',
+    '.jpg',
+    '.jpeg',
+    '.gif',
+    '.gifv',
+    '.pdf',
+    '.mp4',
+    '.mp3',
+    '.ogv',
+    '.webm',
+)
+
+
+def should_exclude(url):
+    ext = tldextract.extract(url)
+    domain = '.'.join([x for x in ext if x])
+    basedomain = '.'.join(ext[-2:])
+
+    if basedomain in exclude_domains or domain in exclude_domains:
+        return True
+
+    if url.split('?')[0].endswith(exclude_extensions):
+        return True
+
+    return False
+
+
+if __name__ == '__main__':
+    with open('urls.txt') as urls, open('urls-filtered.txt', 'w') as out:
+        for line in tqdm.tqdm(list(urls)):
+            line = line.strip()
+            if should_exclude(line):
+                continue
+
+            out.write(line + '\n')
