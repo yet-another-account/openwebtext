c9bea927110a034a179140e2318aaa3c31dd07ef
eukaryote <eukaryote31@gmail.com>
2019-03-02 21:07:18 -0700

    Exclude ftp and localhost
    We don't really want the scraper hitting localhost or some random ftp server somehow.

--- filter.py
+++ filter.py
@@ -4,9 +4,8 @@ import re
 
 # https://stackoverflow.com/questions/7160737/python-how-to-validate-a-url-in-python-malformed-or-not
 url_regex = re.compile(
-		r'^(?:http|ftp)s?://' # ftp:// http:// or https://
+		r'^(?:http)s?://' # http:// or https://
 		r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-		r'localhost|' #localhost...
 		r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
 		r'(?::\d+)?' # optional port
 		r'(?:/?|[/?]\S+)$', re.IGNORECASE)
